f6ed9fb6ffde68756729eb1a2b34c3659bfb9272
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-12-10 21:16:24 +0200

    9.6 Eslint and 9.7 WebExercises

--- ex9.1-9.7/exerciseCalculator.ts
+++ ex9.1-9.7/exerciseCalculator.ts
@@ -1,54 +1,50 @@
-// interface IResult {
-//   periodLength: number;
-//   trainingDays: number;
-//   success: boolean;
-//   rating: number;
-//   ratingDescription: string;
-//   target: number;
-//   average: number;
-// }
-//
-// function parseArguments(args: Array<string>): Array<number> {
-//   if (args.length < 12) throw new Error("Not enough arguments");
-//   if (args.length > 12) throw new Error("Too many arguments");
-//
-//   return args.slice(2).map((element) => {
-//     if (isNaN(Number(element))) {
-//       throw new Error("Provided values were not numbers!");
-//     }
-//     return Number(element);
-//   });
-// }
-//
-// function exerciseCalculator(inputData: number[]): IResult {
-//   const target = inputData[0];
-//   const data = inputData.slice(1);
-//
-//   const average = data.reduce((acc, hours) => acc + hours, 0) / data.length;
-//
-//   return {
-//     periodLength: data.length,
-//     trainingDays: data.filter((day) => day > 0).length,
-//     success: average >= target,
-//     rating: average >= target ? 3 : average >= target - 1 ? 2 : 1,
-//     ratingDescription:
-//       average >= target
-//         ? "well done!"
-//         : average >= target - 1
-//         ? "not too bad but could be better"
-//         : "better luck next time",
-//     target,
-//     average,
-//   };
-// }
-//
-// try {
-//   const inputData = parseArguments(process.argv);
-//   console.log(exerciseCalculator(inputData));
-// } catch (error) {
-//   let errorMessage = "Something bad happened.";
-//   if (error instanceof Error) {
-//     errorMessage += " Error: " + error.message;
-//   }
-//   console.log(errorMessage);
-// }
+interface IResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+export function parseExerciseArguments(args: Array<string>, target: string) {
+  if (isNaN(Number(target))) {
+    throw new Error("malformatted parameters");
+  }
+
+  if (!Array.isArray(args)) {
+    throw new Error("malformatted parameters");
+  }
+
+  const parsedArray = args.map((element) => {
+    if (isNaN(Number(element))) {
+      throw new Error("malformatted parameters");
+    }
+    return Number(element);
+  });
+
+  return {
+    parsedArray,
+    parsedTarget: Number(target),
+  };
+}
+
+export function exerciseCalculator(data: number[], target: number): IResult {
+  const average = data.reduce((acc, hours) => acc + hours, 0) / data.length;
+
+  return {
+    periodLength: data.length,
+    trainingDays: data.filter((day) => day > 0).length,
+    success: average >= target,
+    rating: average >= target ? 3 : average >= target - 1 ? 2 : 1,
+    ratingDescription:
+      average >= target
+        ? "well done!"
+        : average >= target - 1
+        ? "not too bad but could be better"
+        : "better luck next time",
+    target,
+    average,
+  };
+}
--- ex9.1-9.7/index.ts
+++ ex9.1-9.7/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import { calculateBmi, parseArguments } from "./calculateBmi";
+import {
+  exerciseCalculator,
+  parseExerciseArguments,
+} from "./exerciseCalculator";
+
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
@@ -15,10 +21,34 @@ app.get("/bmi", (req, res) => {
       const result = calculateBmi(parsedHeight, parsedWeight);
       return res.status(200).json(result);
     } catch (e) {
-      return res.send({ error: e.message });
+      const error = (e as Error).message;
+      return res.json({ error });
     }
   }
   return res.send({ error: "malformatted parameters" });
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-assignment
+  const { dailyExercises, target }: any = req.body;
+
+  if (!dailyExercises || !target) {
+    return res.status(400).json({ error: "parameters missing" });
+  }
+
+  try {
+    const { parsedArray, parsedTarget } = parseExerciseArguments(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      dailyExercises,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      target
+    );
+    const result = exerciseCalculator(parsedArray, parsedTarget);
+    return res.status(200).json(result);
+  } catch (e) {
+    const error = (e as Error).message;
+    return res.json({ error });
+  }
 });
 
 const PORT = 3002;
